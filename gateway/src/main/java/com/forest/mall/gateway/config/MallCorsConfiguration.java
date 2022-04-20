4169bbafa4adfc925de5993642d9ae0eb53c43c0
forest <kuangyl0212@gmail.com>
2022-04-20 23:11:25 +0800

    feat(category): implement category deletion

---
+++ gateway/src/main/java/com/forest/mall/gateway/config/MallCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.forest.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * @Author forest
+ * @Date 2022/4/19 21:12
+ * @Version 1.0
+ */
+@Configuration
+public class MallCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
+    }
+}
